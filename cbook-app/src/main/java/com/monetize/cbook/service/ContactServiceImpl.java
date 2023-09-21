6ca81d8fe29176ca0ed6942c47a669702e1665fd
lakshmi narayana <lakshmi.narayana@monetize360.io>
2023-09-21 15:49:32 +0530

    Added contact service impl

---
+++ cbook-app/src/main/java/com/monetize/cbook/service/ContactServiceImpl.java
@@ -0,0 +1,38 @@
+package com.monetize.cbook.service;
+
+import com.monetize.cbook.domain.Contact;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ContactServiceImpl implements  ContactService{
+  @Override
+  public Contact addContact(Contact contact) {
+    return null;
+  }
+
+  @Override
+  public List<Contact> getContacts() {
+    return null;
+  }
+
+  @Override
+  public List<Contact> searchContact(String str) {
+    return null;
+  }
+
+  @Override
+  public Contact getContact(UUID id) {
+    return null;
+  }
+
+  @Override
+  public Contact updateContact(Contact contact) {
+    return null;
+  }
+
+  @Override
+  public boolean deleteContact(UUID id) {
+    return false;
+  }
+}
